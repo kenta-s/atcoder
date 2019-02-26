371daeb331167af2cb49bfac7e1c1366cb3321f1
kenta-s <knt01222@gmail.com>
2019-02-26 23:46:16 +0900

    abc119_d

--- abc119/d.rb
+++ abc119/d.rb
@@ -2,16 +2,13 @@ a,b,q = gets.split.map(&:to_i)
 jinja = []
 tera = []
 xs = []
-hoge = []
 a.times do
   j = gets.to_i
   jinja << j
-  hoge << [j, "j"]
 end
 b.times do
   t = gets.to_i
   tera << t
-  hoge << [t, "t"]
 end
 q.times do
   xs << gets.to_i
@@ -19,9 +16,72 @@ end
 
 jinja.sort!
 tera.sort!
-hoge.sort!{|a,b| a[0] <=> b[0]}
 
-xs.each do |x|
+def search_low(current, j_or_t)
+  i = j_or_t.bsearch_index{|j| j > current}
+  if i.nil?
+    i = -1
+  elsif i != 0
+    i = i-1
+  elsif i == 0
+    return [nil, Float::INFINITY]
+  end
+  [j_or_t[i], (current-j_or_t[i]).abs]
 end
 
-p hoge
+def search_high(current, j_or_t)
+  i = j_or_t.bsearch_index{|j| j > current}
+  if i.nil?
+    i = -1
+  end
+  [j_or_t[i], (current-j_or_t[i]).abs]
+end
+
+def hoge1(x, jinja, tera)
+  current1, cost1 = search_low(x, jinja)
+  return Float::INFINITY if current1.nil?
+  current2, cost2 = search_low(current1, tera)
+  cost2 = Float::INFINITY if current2.nil?
+  current3, cost3 = search_high(current1, tera)
+  cost3 = Float::INFINITY if current3.nil?
+  [cost1+cost2, cost1+cost3].min
+end
+
+def hoge2(x, jinja, tera)
+  current1, cost1 = search_high(x, jinja)
+  return Float::INFINITY if current1.nil?
+  current2, cost2 = search_low(current1, tera)
+  cost2 = Float::INFINITY if current2.nil?
+  current3, cost3 = search_high(current1, tera)
+  cost3 = Float::INFINITY if current3.nil?
+  [cost1+cost2, cost1+cost3].min
+end
+
+def hoge3(x, jinja, tera)
+  current1, cost1 = search_low(x, tera)
+  return Float::INFINITY if current1.nil?
+  current2, cost2 = search_low(current1, jinja)
+  cost2 = Float::INFINITY if current2.nil?
+  current3, cost3 = search_high(current1, jinja)
+  cost3 = Float::INFINITY if current3.nil?
+  [cost1+cost2, cost1+cost3].min
+end
+
+def hoge4(x, jinja, tera)
+  current1, cost1 = search_high(x, tera)
+  return Float::INFINITY if current1.nil?
+  current2, cost2 = search_low(current1, jinja)
+  cost2 = Float::INFINITY if current2.nil?
+  current3, cost3 = search_high(current1, jinja)
+  cost3 = Float::INFINITY if current3.nil?
+  [cost1+cost2, cost1+cost3].min
+end
+
+xs.each do |x|
+  puts [
+    hoge1(x, jinja, tera),
+    hoge2(x, jinja, tera),
+    hoge3(x, jinja, tera),
+    hoge4(x, jinja, tera)
+  ].min
+end
