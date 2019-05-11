aad8f0cecdd722ac52a0bbf9bd08712441cfd1a1
kenta-s <knt01222@gmail.com>
2019-05-12 00:30:11 +0900

    abc104d

--- abc104/d01.rb
+++ abc104/d01.rb
@@ -14,21 +14,18 @@ s.each.with_index(1) do |c,i|
   dp[i][1] = dp[i-1][1]
   dp[i][2] = dp[i-1][2]
   dp[i][3] = dp[i-1][3]
-  dp[i][0] += dp[i-1][0]
-  dp[i][1] += dp[i-1][1]
-  dp[i][2] += dp[i-1][2]
-  dp[i][3] += dp[i-1][3]
   case c
   when "A"
     dp[i][1] += dp[i-1][0]
   when "B"
     dp[i][2] += dp[i-1][1]
-    dp[i][0] += dp[i-1][0]
   when "C"
     dp[i][3] += dp[i-1][2]
-    dp[i][0] += dp[i-1][0]
   when "?"
-    dp[i][0] += dp[i-1][0]
+    dp[i][0] += dp[i-1][0] * 2
+    dp[i][1] += dp[i-1][1] * 2
+    dp[i][2] += dp[i-1][2] * 2
+
     dp[i][1] += dp[i-1][0]
     dp[i][2] += dp[i-1][1]
     dp[i][3] += dp[i-1][2]
@@ -36,5 +33,8 @@ s.each.with_index(1) do |c,i|
     raise "okashi-zo"
   end
 end
+
+require 'pry-byebug'
+binding.pry
 
 puts dp[s.size][3]
