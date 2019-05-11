4e02d5fddab4dd9fc03f44b1c1bc8f7712b7c5cb
kenta-s <knt01222@gmail.com>
2019-05-11 22:34:34 +0900

    abc104d

--- abc104/d01.rb
+++ abc104/d01.rb
@@ -14,14 +14,21 @@ s.each.with_index(1) do |c,i|
   dp[i][1] = dp[i-1][1]
   dp[i][2] = dp[i-1][2]
   dp[i][3] = dp[i-1][3]
+  dp[i][0] += dp[i-1][0]
+  dp[i][1] += dp[i-1][1]
+  dp[i][2] += dp[i-1][2]
+  dp[i][3] += dp[i-1][3]
   case c
   when "A"
     dp[i][1] += dp[i-1][0]
   when "B"
     dp[i][2] += dp[i-1][1]
+    dp[i][0] += dp[i-1][0]
   when "C"
     dp[i][3] += dp[i-1][2]
+    dp[i][0] += dp[i-1][0]
   when "?"
+    dp[i][0] += dp[i-1][0]
     dp[i][1] += dp[i-1][0]
     dp[i][2] += dp[i-1][1]
     dp[i][3] += dp[i-1][2]
