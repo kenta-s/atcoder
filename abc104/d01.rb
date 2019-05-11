a556043d41d42d71d5b00fce332de3743b84bc48
kenta-s <knt01222@gmail.com>
2019-05-11 20:50:25 +0900

    abc104d

---
+++ abc104/d01.rb
@@ -0,0 +1,33 @@
+s = gets.chomp.chars
+dp = {
+  0 => {
+    0 => 1,
+    1 => 0,
+    2 => 0,
+    3 => 0
+  }
+}
+
+s.each.with_index(1) do |c,i|
+  dp[i] ||= {}
+  dp[i][0] = dp[i-1][0]
+  dp[i][1] = dp[i-1][1]
+  dp[i][2] = dp[i-1][2]
+  dp[i][3] = dp[i-1][3]
+  case c
+  when "A"
+    dp[i][1] += dp[i-1][0]
+  when "B"
+    dp[i][2] += dp[i-1][1]
+  when "C"
+    dp[i][3] += dp[i-1][2]
+  when "?"
+    dp[i][1] += dp[i-1][0]
+    dp[i][2] += dp[i-1][1]
+    dp[i][3] += dp[i-1][2]
+  else
+    raise "okashi-zo"
+  end
+end
+
+puts dp[s.size][3]
