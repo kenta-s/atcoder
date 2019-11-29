f2fbc78063067e2abef2819f63b98a5d35fa0fa3
kenta-s <kenta.shirai@ascenderhcm.com>
2019-11-29 20:17:24 +0900

    update dfs

--- typical_content001/dfs.cpp
+++ typical_content001/dfs.cpp
@@ -9,6 +9,26 @@ void dfs(int x, int y){
 };
 
 int main(){
+  int i,j,x,y,xg,yg;
 	cin >> H >> W;
-  cout << "No" << endl;
+  for(i=0; i<H; i++){
+    for(j=0; j<W; j++){
+      cin >> c[i][j];
+      if(c[i][j] == 's'){
+        x = i;
+        y = j;
+      }
+      if(c[i][j] == 'g'){
+        xg = i;
+        yg = j;
+      }
+    }
+  }
+  dfs(x,y);
+
+  if(visited[xg][xy] == 1){
+    cout << "Yes" << endl;
+  }else{
+    cout << "No" << endl;
+  }
 }
