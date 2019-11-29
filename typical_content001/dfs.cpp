8ce5cdd9fd35f967395f5d4e8e0872adbeda2ca2
kenta-s <kenta.shirai@ascenderhcm.com>
2019-11-29 20:11:09 +0900

    dfs.cpp

---
+++ typical_content001/dfs.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int H, W, visited[500][500];
+char c[500][500];
+
+// TODO: implement
+void dfs(int x, int y){
+};
+
+int main(){
+	cin >> H >> W;
+  cout << "No" << endl;
+}
