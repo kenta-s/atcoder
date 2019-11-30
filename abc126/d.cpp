d4f3a5a0dba19967ccfe4f2caa73aefe9af7b7cb
kenta-s <knt01222@gmail.com>
2019-11-30 22:35:04 +0900

    update

---
+++ abc126/d.cpp
@@ -0,0 +1,30 @@
+// TODO
+#include <iostream>
+
+using namespace std;
+
+int N;
+vector<int> parent;
+
+int root(int x){
+}
+
+void unite(int x, int y){
+}
+
+int main(){
+  cin >> N;
+  parent.resize(N+1);
+  int i,a,b,c;
+  for(i=0; i<=N; i++){
+    parent[i] = i;
+  }
+  for(i=0; i<N; i++){
+    cin >> a >> b >> w;
+    if(w % 2 == 0){
+    }else{
+    }
+  }
+  return 0;
+}
+
