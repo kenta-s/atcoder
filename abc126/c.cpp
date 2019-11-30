9c537d115ecc5517005ea3cdfcd4d878c9a1cfe5
kenta-s <knt01222@gmail.com>
2019-11-30 19:47:31 +0900

    abc126 c

---
+++ abc126/c.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+int N,K;
+
+int dfs(int n, double depth){
+  if(n >= K){
+    return depth;
+  }else{
+    return dfs(n*2, depth + 1);
+  }
+}
+
+int main(){
+  cin >> N >> K;
+  int i;
+  double ans = 0;
+  for(i=0; i<N; i++){
+    if(i+1 >= K){
+      ans += 1;
+    }else{
+      ans += (1 / pow(2, dfs(i+1, 0)));
+    }
+  }
+  cout << setprecision(10) << ans / N << endl;
+  return 0;
+}
