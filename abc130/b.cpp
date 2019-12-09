7292e003e3644d63a1e056efe773c23c44a7e70f
kenta-s <knt01222@gmail.com>
2019-12-09 23:46:45 +0900

    abc130 b

---
+++ abc130/b.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int N,X;
+  cin >> N >> X;
+  int L[N];
+  int A[N+1];
+  A[0] = 0;
+  for(int i=0; i<N; i++){
+    cin >> L[i];
+    if(i==0){
+      A[i+1] = L[i];
+      // cout << A[i+1] << endl;
+    }else{
+      A[i+1] = A[i] + L[i];
+      // cout << A[i+1] << endl;
+    }
+  }
+
+  for(int i=0; i<=N; i++){
+    if(A[i] > X){
+      cout << i << endl;
+      break;
+    }
+  }
+}
