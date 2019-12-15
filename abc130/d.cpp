3712a21e3a9c77d79c01addac0a929b6078fd205
kenta-s <knt01222@gmail.com>
2019-12-15 11:06:36 +0900

    abc130 d TLE

--- abc130/d.cpp
+++ abc130/d.cpp
@@ -7,25 +7,38 @@ int main(){
   int N, K;
   cin >> N >> K;
   vector<int> A(N);
+  long long sum = 0;
   for(int i=0; i<N; i++){
     cin >> A[i];
+    // sum += A[i];
   }
   int ans = 0;
-  long long sum = 0;
   int L = 0;
-  for(int R=0; R<N; R++){
-    sum += A[R];
-    if(sum >= K && L == R){
-      ans++;
-      continue;
-    }
-
-    while(sum >= K && L < R){
-      ans++;
-      sum -= A[L];
-      L++;
+  int R = 0;
+  for(int L=0; L<N; L++){
+    int tmp = 0;
+    for(int R=L; R<N; R++){
+      tmp += A[R];
+      if(tmp >= K){
+        ans += N - R;
+        // cout << "L is " << L << ' ' << "R is " << R << ' ' << "ans is now " << ans << endl;
+        break;
+      }
     }
   }
+  // for(int R=0; R<N; R++){
+  //   sum += A[R];
+  //   if(sum >= K && L == R){
+  //     ans++;
+  //     continue;
+  //   }
+
+  //   while(sum >= K && L < R){
+  //     ans++;
+  //     sum -= A[L];
+  //     L++;
+  //   }
+  // }
   cout << ans << endl;
   return 0;
 }
