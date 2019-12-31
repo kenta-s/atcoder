6e6a0ed7156afe6442add0cef1d056e5f535ef31
kenta-s <knt01222@gmail.com>
2019-12-31 17:38:39 +0900

    abc137 c

---
+++ abc137/c.rb
@@ -0,0 +1,25 @@
+n = gets.to_i
+ss = []
+n.times do
+  s = gets.chomp.chars.sort.join
+  ss << s
+end
+
+ss.sort!
+
+ans = 0
+h = Hash.new(0)
+ss.size.times do |i|
+  h[ss[i]] += 1
+end
+
+h.each do |k,v|
+  if v > 1
+    ans += (1..v).to_a.combination(2).size
+  else
+    ans += v
+  end
+end
+
+# puts ss.size - ss.uniq.size
+puts ans
