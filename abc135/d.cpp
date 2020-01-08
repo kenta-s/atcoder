d05af97ad825dcc82f2dc6d7f2747824c6713207
kenta-s <knt01222@gmail.com>
2020-01-08 23:55:10 +0900

    abc135 d

--- abc135/d.cpp
+++ abc135/d.cpp
@@ -6,20 +6,21 @@ using namespace std;
 int main(){
   string S;
   cin >> S;
-  int N = S.size();
+  long long N = S.size();
+  long long mod = pow(10, 9) + 7;
 
   long long dp[N][13];
-  for(int i=0; i<N; i++){
-    for(int j=0; j<13; j++){
+  for(long long i=0; i<N; i++){
+    for(long long j=0; j<13; j++){
       dp[i][j] = 0;
     }
   }
 
-  int digit = 10;
-  for(int i=N-1; i>=0; i--){
+  long long digit = 10;
+  for(long long i=N-1; i>=0; i--){
     if(i == N-1){
       if(S[i] == '?'){
-        for(int j=0; j<10; j++){
+        for(long long j=0; j<10; j++){
           dp[i][j % 13] += 1;
         }
       }else{
@@ -30,22 +31,19 @@ int main(){
     }
 
     if(S[i] == '?'){
-      for(int j=0; j<10; j++){
-        int n = (j * digit) % 13;
-        dp[i][n] += dp[i+1][(18 - n) % 13] + 1;
+      for(long long j=0; j<13; j++){
+        for(long long k=0; k<10; k++){
+          dp[i][(j + (digit * k)) % 13] = (dp[i][(j + (digit * k)) % 13] + dp[i+1][j]) % mod;
+        }
       }
     }else{
-      int n = ((S[i] - '0') * digit) % 13;
-      cout << " n is " << n << endl;
-      for(int j=0; j<13; j++){
-        dp[i][j] += dp[i+1][j];
+      long long current = (S[i] - '0') * digit;
+      for(long long j=0; j<13; j++){
+        dp[i][(j + current) % 13] = (dp[i][(j + current) % 13] + dp[i+1][j]) % mod;
       }
-      cout << " dp is " << dp[i][n] << endl;
     }
     digit = digit * 10;
   }
-
-  long long mod = pow(10, 9) + 7;
 
   cout << dp[0][5] % mod << endl;
 
